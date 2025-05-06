1a56ff61e6ecbd5438a9f0b6ac5c40c42f968225
IbrahimBakry <160511424+IbrahimBakry@users.noreply.github.com>
2025-05-06 11:20:14 +0400

    Add files via upload

---
+++ Altitude-Linear-Changed-NewModel_PUBLIC_V1.1/NewModel_PUBLIC/ALT_observer.m
@@ -0,0 +1,24 @@
+function [out] = ALT_observer( in )
+glob;
+
+estz=in(1);
+U1=in(2);
+
+phi=in(3);
+theta=in(4);
+psi=in(10);
+z=in(5);
+estdotz=in(11);
+
+% friction force estimation (propellers friction+OS4 center friction) [N]
+ZaF = 0.5*Cz*A*rho*estdotz*abs(estdotz)*P + 0.5*Cz*Ac*rho*estdotz*abs(estdotz);  
+
+% Observer Gain;
+L=12;   
+
+% estddotz=+g-((cos(phi)*cos(theta))/m)*U1+L*(z-estz);
+estddotz=-g*cos(phi)*cos(theta)+(cos(psi)*cos(phi)*U1-ZaF+in(9))/m + L*(z-estz);
+
+out(1)=estddotz;
+out(2)=(z-estz);
+
